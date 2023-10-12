2ae6c213f0b8ab353b71885b31f802018f533bb8
Ariel2711 <arielnaviandanaputra@gmail.com>
2023-10-12 17:47:40 +0700

    some update

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, avoid_print
+// ignore_for_file: prefer_const_constructors, avoid_print, camel_case_types, must_be_immutable
 import 'package:basic_ui/pages/login.dart';
 import 'package:flutter/material.dart';
 
@@ -90,170 +90,72 @@ class _HomePageState extends State<HomePage> {
                   scrollDirection: Axis.horizontal,
                   itemCount: 10,
                   itemBuilder: (context, index) {
-                    return Container(
-                      margin: EdgeInsets.all(10),
-                      height: 100,
-                      width: 100,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8),
-                          color: Colors.white,
-                          boxShadow: const [
-                            BoxShadow(
-                              blurRadius: 2,
-                              color: Color.fromARGB(255, 223, 222, 222),
-                            )
-                          ]),
-                      child: Center(child: Text("Box ${index + 1}")),
-                    );
+                    return boxWidget(text: "Box ${index + 1}");
                   },
                 ),
               ),
               SizedBox(
                 height: 20,
               ),
-              Container(
-                height: 70,
-                // width: 200,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.white,
-                    boxShadow: const [
-                      BoxShadow(
-                        blurRadius: 2,
-                        color: Color.fromARGB(255, 223, 222, 222),
-                      )
-                    ]),
-                child: Center(
-                  child: GestureDetector(
-                    onTap: () {
-                      showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                              title: Text("Judul dialog"),
-                              content: Text("Isi dialog"),
-                            );
-                          });
-                    },
-                    child: Container(
-                      width: 150,
-                      height: 40,
-                      padding: EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.circular(8)),
-                      child: Center(
-                          child: Text(
-                        'Show Dialog',
-                        style: TextStyle(color: Colors.white, fontSize: 16),
-                      )),
-                    ),
-                  ),
-                ),
+              buttonCard(
+                text: "Show Dialog",
+                callback: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text("Judul dialog"),
+                          content: Text("Isi dialog"),
+                        );
+                      });
+                },
               ),
               SizedBox(
                 height: 20,
               ),
-              Container(
-                height: 70,
-                // width: 200,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.white,
-                    boxShadow: const [
-                      BoxShadow(
-                        blurRadius: 2,
-                        color: Color.fromARGB(255, 223, 222, 222),
-                      )
-                    ]),
-                child: Center(
-                  child: GestureDetector(
-                    onTap: () {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text("Judul Snackbar"),
-                        duration: Duration(seconds: 2),
-                        action: SnackBarAction(
-                          label: "Close",
-                          onPressed: () {},
-                        ),
-                      ));
-                    },
-                    child: Container(
-                      width: 150,
-                      height: 40,
-                      padding: EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.circular(8)),
-                      child: Center(
-                          child: Text(
-                        'Show Snackbar',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: Colors.white, fontSize: 16),
-                      )),
-                    ),
-                  ),
-                ),
-              ),
+              buttonCard(
+                  text: "Show Snackbar",
+                  callback: () {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text("Judul Snackbar"),
+                      duration: Duration(seconds: 2),
+                      action: SnackBarAction(
+                        label: "Close",
+                        onPressed: () {},
+                      ),
+                    ));
+                  }),
               SizedBox(
                 height: 20,
               ),
-              Container(
-                height: 70,
-                // width: 200,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.white,
-                    boxShadow: const [
-                      BoxShadow(
-                        blurRadius: 2,
-                        color: Color.fromARGB(255, 223, 222, 222),
-                      )
-                    ]),
-                child: Center(
-                  child: GestureDetector(
-                    onTap: () {
-                      showModalBottomSheet(
-                          context: context,
-                          builder: (context) {
-                            return Container(
-                              decoration: BoxDecoration(color: Colors.white),
-                              height: 200,
-                              child: ListView(
-                                children: const [
-                                  ListTile(
-                                    title: Text("Menu 1"),
-                                    leading: Icon(Icons.add),
-                                  ),
-                                  ListTile(
-                                    title: Text("Menu 2"),
-                                    leading: Icon(Icons.edit),
-                                  ),
-                                  ListTile(
-                                    title: Text("Menu 3"),
-                                    leading: Icon(Icons.search),
-                                  )
-                                ],
+              buttonCard(
+                text: "Show Bottom Sheet",
+                callback: () {
+                  showModalBottomSheet(
+                      context: context,
+                      builder: (context) {
+                        return Container(
+                          decoration: BoxDecoration(color: Colors.white),
+                          height: 200,
+                          child: ListView(
+                            children: const [
+                              ListTile(
+                                title: Text("Menu 1"),
+                                leading: Icon(Icons.add),
                               ),
-                            );
-                          });
-                    },
-                    child: Container(
-                      width: 150,
-                      height: 50,
-                      padding: EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.circular(8)),
-                      child: Center(
-                          child: Text(
-                        'Show Bottom Sheet',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: Colors.white, fontSize: 16),
-                      )),
-                    ),
-                  ),
-                ),
+                              ListTile(
+                                title: Text("Menu 2"),
+                                leading: Icon(Icons.edit),
+                              ),
+                              ListTile(
+                                title: Text("Menu 3"),
+                                leading: Icon(Icons.search),
+                              )
+                            ],
+                          ),
+                        );
+                      });
+                },
               ),
             ],
           ),
@@ -274,6 +176,71 @@ class _HomePageState extends State<HomePage> {
             }
           });
         },
+      ),
+    );
+  }
+}
+
+class boxWidget extends StatelessWidget {
+  boxWidget({super.key, this.text});
+  String? text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      height: 100,
+      width: 100,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.white,
+          boxShadow: const [
+            BoxShadow(
+              blurRadius: 2,
+              color: Color.fromARGB(255, 223, 222, 222),
+            )
+          ]),
+      child: Center(child: Text(text ?? '')),
+    );
+  }
+}
+
+class buttonCard extends StatelessWidget {
+  buttonCard({super.key, this.text, this.callback});
+  String? text;
+  void Function()? callback;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.white,
+          boxShadow: const [
+            BoxShadow(
+              blurRadius: 2,
+              color: Color.fromARGB(255, 223, 222, 222),
+            )
+          ]),
+      child: Center(
+        child: GestureDetector(
+          onTap: callback ?? () {},
+          child: Container(
+            width: 180,
+            height: 40,
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+                color: Colors.blue, borderRadius: BorderRadius.circular(8)),
+            child: Center(
+                child: Text(
+              text ?? '',
+              textAlign: TextAlign.center,
+              maxLines: 2,
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            )),
+          ),
+        ),
       ),
     );
   }
